70918144f5fd6bdea003926180c0f49fb208c703
Amanbek Asylbekov <amanbek_asylbekov@epam.com>
2022-08-09 19:53:01 +0600

    feat: create new pizza or drink

--- src/pages/CreateNewElement/CreateNewElement.jsx
+++ src/pages/CreateNewElement/CreateNewElement.jsx
@@ -1,19 +1,93 @@
+import { useState } from "react";
+import css from "./CreateNewElement.module.css";
 
 function CreateNewElement() {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState("pizza");
+  const [isSending, setSending] = useState(false);
+
+  console.log(status);
+
+  const submit = (e) => {
+    e.preventDefault();
+    setSending(true)
+    const data = {
+      title,
+      price,
+      description,
+      img: image,
+    };
+    console.log(data);
+    fetch("https://605b21f027f0050017c063b9.mockapi.io/api/v3/" + status, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .finally(() => {
+        setSending(false)
+    })
+    .then(()=> {
+      // TODO: Автоматически должны перенапрвляться на страницу админа
+
+    })
+  };
   return (
     <div className="container page">
-        <h1 className="text-center">Create new Element</h1>
-        <form>
-            <input type="text" placeholder="Title" />
-            <input type="text" placeholder="Price" />
-            <input type="text" placeholder="Image" />
-            <textarea placeholder="Description"></textarea>
-            <input type="radio" />
-            <input type="radio" />
-            <button className="btn">Create</button>
-        </form>
+      <h1 className="text-center">Create new Element</h1>
+      <form onSubmit={submit} className={css.formWrapper}>
+        <input
+        required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Title"
+        />
+        <input
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          type="number"
+          placeholder="Price"
+        />
+        <input
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          type="url"
+          placeholder="Image Link"
+        />
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+        ></textarea>
+        <label>
+          <input
+            name="status"
+            value="pizza"
+            checked={status === "pizza"}
+            type="radio"
+            onChange={(e) => setStatus(e.target.value)}
+          />
+          <span>Pizza</span>
+        </label>
+        <label>
+          <input
+            name="status"
+            value="drinks"
+            checked={status === "drinks"}
+            type="radio"
+            onChange={(e) => setStatus(e.target.value)}
+          />
+          <span>Drink</span>
+        </label>
+        <button disabled={isSending} className="btn">Create</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default CreateNewElement;
